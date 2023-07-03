82069488578cb2fda249d348b27155fcad790ce4
NathanNguyen <khanhngoc0102@gmail.com>
2023-07-03 15:05:17 -0700

    gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Gallery = () => {
   return (
     <div id="gallery" className="max-w-[1140px]  m-auto w-full p-4 py-16">
-      <h2 className="text-center ">asdasdsary</h2>
+      <h2 className="text-center ">asdasasdsary</h2>
       <div className="grid sm:grid-cols-5 gap-4">
         <div className="sm:col-span-3 col-span-2 row-span-2">
           <img
